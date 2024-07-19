51bd1a99b8b9b9efb62139d9c9d79a22ccfd860d
god464 <god464@users.noreply.github.com>
2024-07-20 00:54:12 +0800

    using flake-parts to refactor
    update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,45 +13,47 @@
     };
   };
   outputs =
-    inputs@{ nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      devShells."${system}".vterm =
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        pkgs.mkShell {
-          packages = with pkgs; [
-            gnumake
-            cmake
-            gcc
-            libtool
-            emacs
-          ];
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      flake = {
+        nixosConfigurations = {
+          router = nixpkgs.lib.nixosSystem {
+            modules = [
+              inputs.disko.nixosModules.disko
+              inputs.sops-nix.nixosModules.sops
+              ./modules
+              ./disko/server.nix
+              ./host/router
+
+            ];
+          };
+          builder = nixpkgs.lib.nixosSystem {
+            modules = [
+              inputs.disko.nixosModules.disko
+              inputs.sops-nix.nixosModules.sops
+              inputs.home-manager.nixosModules.home-manager
+              ./modules
+              ./disko/desktop.nix
+              ./host/builder
+            ];
+          };
         };
-      formatter."${system}" = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-      nixosConfigurations.router = nixpkgs.lib.nixosSystem {
-        specialArgs.inputs = inputs;
-        modules = [
-          inputs.disko.nixosModules.disko
-          inputs.sops-nix.nixosModules.sops
-          ./modules
-          ./disko/server.nix
-          ./host/router
-        ];
       };
-      nixosConfigurations.builder = nixpkgs.lib.nixosSystem {
-        specialArgs.inputs = inputs;
-        modules = [
-          inputs.disko.nixosModules.disko
-          inputs.sops-nix.nixosModules.sops
-          inputs.home-manager.nixosModules.home-manager
-          ./modules
-          ./disko/desktop.nix
-          ./host/builder
-        ];
-      };
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.vterm = pkgs.mkShell {
+            packages = with pkgs; [
+              cmake
+              emacs
+              gcc
+              gnumake
+              libtool
+            ];
+          };
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+
     };
 }
