c66fc8455f3628f28e9c4a18da68e09b1e7a4e53
god464 <god464@users.noreply.github.com>
2024-08-11 22:01:18 +0800

    adding driver update flake.lock

--- host/common/default.nix
+++ host/common/default.nix
@@ -1,6 +1,13 @@
 {
   imports = [ ./i18n.nix ];
   users.mutableUsers = false;
+  hardware = {
+    cpu.amd.updateMicrocode = true;
+    amdgpu = {
+      amdvlk.enable = true;
+      opencl.enable = true;
+    };
+  };
   zramSwap.enable = true;
   services.btrfs.autoScrub.enable = true;
 }
