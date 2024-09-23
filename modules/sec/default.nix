f20c334373138042d23c263280a83652fec97b89
god464 <god464@users.noreply.github.com>
2024-09-23 10:43:47 +0800

    do not use apparmor

--- modules/sec/default.nix
+++ modules/sec/default.nix
@@ -20,17 +20,6 @@ in
       secrets.passwd.neededForUsers = true;
     };
     services.dbus.apparmor = "enabled";
-    security = {
-      apparmor = {
-        enable = true;
-        killUnconfinedConfinables = true;
-      };
-      sudo = {
-        enable = true;
-        execWheelOnly = true;
-      };
-      auditd.enable = true;
-      audit.enable = true;
-    };
+    security.sudo.execWheelOnly = true;
   };
 }
