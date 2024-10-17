9bbfff705af4e95560a880bbdbd8849806282cda
god464 <god464@users.noreply.github.com>
2024-10-17 14:48:20 +0800

    style(hosts): fix style

--- hosts/default.nix
+++ hosts/default.nix
@@ -9,15 +9,19 @@
           specialArgs = {
             inherit inputs;
           };
-          modules = [
-            inputs.home-manager.nixosModules.home-manager
-            inputs.disko.nixosModules.disko
-            inputs.sops-nix.nixosModules.sops
-            inputs.nixos-cosmic.nixosModules.default
-            inputs.impermanence.nixosModules.impermanence
-            ./${host}
-            ../modules
-          ];
+          modules =
+            with inputs;
+            [
+              home-manager.nixosModules.home-manager
+              disko.nixosModules.disko
+              sops-nix.nixosModules.sops
+              nixos-cosmic.nixosModules.default
+              impermanence.nixosModules.impermanence
+            ]
+            ++ [
+              ./${host}
+              ../modules
+            ];
         };
     in
     {
