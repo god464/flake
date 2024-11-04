299160f8bd78326c5885204e47fbf0645a2a5dad
god464 <god464@users.noreply.github.com>
2024-11-04 10:19:22 +0800

    refactor(fonts): use patched fonts

--- modules/desktop'/fontx.nix
+++ modules/desktop'/fontx.nix
@@ -11,7 +11,7 @@ in
 {
   config = mkIf cfg.enable {
     fonts.packages = with pkgs; [
-      fira-code
+      fira-code-nerdfont
       sarasa-gothic
       source-han-sans
       source-han-serif
