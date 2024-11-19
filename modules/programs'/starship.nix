3d1888d22e1cce0ca6e3e46d9a893bc0048667e8
god464 <god464@users.noreply.github.com>
2024-11-19 11:12:04 +0800

    feat(starship): add more symbols

--- modules/programs'/starship.nix
+++ modules/programs'/starship.nix
@@ -9,6 +9,7 @@ in
       enable = true;
       settings = {
         c.symbol = "󰙱 ";
+        cmake.symbol = "";
         java.symbol = "";
         lua.symbol = "󰢱 ";
         nix_shell.symbol = "󱄅 ";
@@ -18,8 +19,17 @@ in
         directory = {
           read_only = "";
           home_symbol = "";
+          substitutions = {
+            Documents = "󰈙 ";
+            Downloads = " ";
+            Music = " ";
+            Pictures = " ";
+            Videos = " ";
+            Desktop = " ";
+          };
         };
         git_branch.symbol = " ";
+        git_commit.tag_symbol = "  ";
         git_status = {
           conflicted = "󰇼";
           ahead = "󰁝";
