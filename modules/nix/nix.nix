386c52d0167fd11bf422fb2fbbf91615fda848d9
god464 <god464@users.noreply.github.com>
2024-12-13 16:27:46 +0800

    feat: add mirror

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -31,6 +31,7 @@ in
         substituters = mkAfter (
           cfg.cache
           ++ [
+            "https://mirror.sjtu.edu.cn/nix-channels/store"
             "https://cache.nixos.org"
             "https://nix-community.cachix.org"
             "https://cache.garnix.io"
@@ -39,6 +40,7 @@ in
         trusted-public-keys = mkAfter (
           cfg.trustKeys
           ++ [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           ]
