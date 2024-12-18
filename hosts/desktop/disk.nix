9ff756308f89879c9a3756aafc9d4c6a580c473b
god464 <god464@users.noreply.github.com>
2024-12-18 16:42:22 +0800

    fix: fix disk partition

--- hosts/desktop/disk.nix
+++ hosts/desktop/disk.nix
@@ -1,66 +1,64 @@
 {
   disko.devices = {
-    disk = {
-      sda = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "1G";
-              label = "ESP";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+    disk.sda = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "500M";
+            label = "ESP";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
-            root = {
-              size = "100%";
-              label = "ROOT";
+          };
+          root = {
+            size = "100%";
+            label = "ROOT";
+            content = {
+              type = "luks";
+              name = "nixos";
+              settings = {
+                keyFile = "/dev/disk/by-label/Ventoy";
+                keyFileSize = 1024 * 1024;
+                keyFileOffset = 2 * 1024 * 1024;
+                allowDiscards = true;
+                bypassWorkqueues = true;
+              };
               content = {
-                type = "luks";
-                name = "nixos";
-                settings = {
-                  keyFile = "/dev/disk/by-label/Ventoy";
-                  keyFileSize = 1024 * 1024;
-                  keyFileOffset = 2 * 1024 * 1024;
-                  allowDiscards = true;
-                  bypassWorkqueues = true;
-                };
-                content = {
-                  type = "btrfs";
-                  extraArgs = [
-                    "-f"
-                    "--csum XXHASH"
-                    "-L NixOS"
-                  ];
-                  subvolumes = {
-                    "@nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    "@persist" = {
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                      mountpoint = "/persist";
-                    };
-                    "@swap" = {
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                      mountpoint = "/.swap";
-                      swap.swapfile.size = "32G";
-                    };
+                type = "btrfs";
+                extraArgs = [
+                  "-f"
+                  "--csum XXHASH"
+                  "-L NixOS"
+                ];
+                subvolumes = {
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@persist" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/persist";
+                  };
+                  "@swap" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/.swap";
+                    swap.swapfile.size = "32G";
                   };
                 };
               };
--- hosts/server/disk.nix
+++ hosts/server/disk.nix
@@ -1,34 +1,51 @@
 {
   disko.devices = {
-    disk = {
-      sda = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "500M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+    disk.sda = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "500M";
+            label = "ESP";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [
-                  "-f"
-                  "-L NixOS"
-                ];
-                mountpoint = "/";
-                mountOptions = [
-                  "compress=zstd"
-                  "noatime"
-                ];
+          };
+          root = {
+            size = "100%";
+            label = "ROOT";
+            content = {
+              type = "btrfs";
+              extraArgs = [
+                "-f"
+                "-L NixOS"
+              ];
+              mountpoint = "/";
+              mountOptions = [
+                "compress=zstd"
+                "noatime"
+              ];
+              subvolumes = {
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                "@var" = {
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                  mountpoint = "/var";
+                };
               };
             };
           };
