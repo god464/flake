63f7645901091c6b7cebc64f4698c3180e78765d
god464 <god464@users.noreply.github.com>
2025-01-11 17:41:47 +0800

    fix: fix fonts fallback

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -10,21 +10,48 @@ let
 in
 {
   config = mkIf cfg.enable {
-    fonts.packages = with pkgs; [
-      fira-code
-      sarasa-gothic
-      source-han-sans
-      source-han-serif
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      jigmo
-      noto-fonts
-      noto-fonts-extra
-      source-serif-pro
-      source-sans-pro
-      nerd-fonts.symbols-only
-      twemoji-color-font
-      font-awesome
-    ];
+    fonts = {
+      packages = with pkgs; [
+        fira-code
+        sarasa-gothic
+        source-han-sans
+        source-han-serif
+        source-han-mono
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        jigmo
+        noto-fonts
+        noto-fonts-extra
+        source-serif-pro
+        source-sans-pro
+        nerd-fonts.symbols-only
+        twemoji-color-font
+        font-awesome
+      ];
+      fontconfig = {
+        subpixel.rgba = "rgb";
+        defaultFonts = {
+          serif = [
+            "Source Han Serif SC"
+            "Noto Serif CJK SC"
+            "Symbols Nerd Font"
+          ];
+          sansSerif = [
+            "Source Han Sans SC"
+            "Noto Sans CJK SC"
+            "Symbols Nerd Font"
+          ];
+          monospace = [
+            "Source Han Mono SC"
+            "Noto Sans Mono CJK SC"
+            "Symbols Nerd Font Mono"
+          ];
+          emoji = [
+            "Twitter Color Emoji"
+            "Symbols Nerd Font"
+          ];
+        };
+      };
+    };
   };
 }
