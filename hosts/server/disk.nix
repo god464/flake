4764c220d9f493074caa42a696ac9086cc048e04
god464 <god464@users.noreply.github.com>
2025-02-02 20:39:24 +0800

    fix: fix format

--- hosts/server/disk.nix
+++ hosts/server/disk.nix
@@ -1,59 +1,37 @@
 {
   disko.devices = {
-    disk.sda = {
-      device = "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "500M";
-            label = "ESP";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              name = "ESP";
+              start = "1M";
+              end = "128M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
             };
-          };
-          root = {
-            size = "100%";
-            type = "8304";
-            label = "ROOT";
-            content = {
-              type = "btrfs";
-              mountpoint = "/";
-              mountOptions = [
-                "compress=zstd"
-                "noatime"
-              ];
-              extraArgs = [
-                "-f"
-                "-L NixOS"
-              ];
-              subvolumes = {
-                "@" = {
-                  mountpoint = "/";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@var" = {
-                  mountpoint = "/var";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [
+                  "-f"
+                  "-L Nixos"
+                ];
+                mountpoint = "/";
+                mountOptions = [
+                  "compress=zstd"
+                  "noatime"
+                ];
               };
             };
           };
