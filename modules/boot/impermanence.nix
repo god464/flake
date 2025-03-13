10a9a8b9de699bbe7885b0fcfbe4b679960c76f0
god464 <god464@users.noreply.github.com>
2025-03-13 22:26:31 +0800

    feat: feat

--- modules/boot/impermanence.nix
+++ modules/boot/impermanence.nix
@@ -21,38 +21,60 @@ in
         "/var/cache"
         "/var/log"
       ];
-      users.cl.directories = [
-        {
-          directory = "persist";
-          mode = "700";
-        }
-        {
-          directory = ".gnupg";
-          mode = "700";
-        }
-        {
-          directory = ".ssh";
-          mode = "700";
-        }
-        ".cache/nvim"
-        ".cache/pre-commit"
-        ".cache/treefmt"
-        ".cache/bat"
-        ".cache/nix"
-        ".local/share/atuin"
-        ".local/share/direnv"
-        ".local/share/fish"
-        ".local/share/nvim"
-        ".local/share/zoxide"
-        ".local/share/nix"
-        ".local/state/nix"
-        ".config/fcitx5"
-        ".config/gh"
-        ".config/github-copilot/"
-        ".local/state/wireplumber"
-        ".mozilla"
-        ".thunderbird"
-      ];
+      users = {
+        cl.directories = [
+          {
+            directory = "persist";
+            mode = "700";
+          }
+          {
+            directory = ".gnupg";
+            mode = "700";
+          }
+          {
+            directory = ".ssh";
+            mode = "700";
+          }
+          ".cache/nvim"
+          ".cache/pre-commit"
+          ".cache/treefmt"
+          ".cache/bat"
+          ".cache/nix"
+          ".local/share/atuin"
+          ".local/share/direnv"
+          ".local/share/fish"
+          ".local/share/nvim"
+          ".local/share/zoxide"
+          ".local/share/nix"
+          ".local/state/nix"
+          ".config/fcitx5"
+          ".config/gh"
+          ".local/state/wireplumber"
+          ".mozilla"
+          ".thunderbird"
+        ];
+        elysium.directories = [
+          ".cache/nix"
+          ".local/share/nix"
+          ".local/state/nix"
+          ".local/share/atuin"
+          ".local/share/zoxide"
+          ".mozilla"
+          ".thunderbird"
+          {
+            directory = "persist";
+            mode = "700";
+          }
+          {
+            directory = ".gnupg";
+            mode = "700";
+          }
+          {
+            directory = ".ssh";
+            mode = "700";
+          }
+        ];
+      };
     };
   };
 }
