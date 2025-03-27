f9a185f143af48ffbc7fd1597345d7b789e08370
god464 <god464@users.noreply.github.com>
2025-03-27 09:36:37 +0800

    feat: add another key

--- modules/nix/home-manager.nix
+++ modules/nix/home-manager.nix
@@ -87,11 +87,27 @@ in
             };
           };
           starship.enable = true;
-          gpg.enable = true;
+          gpg = {
+            enable = true;
+            mutableKeys = false;
+            mutableTrust = false;
+          };
           ssh = {
             enable = true;
-            forwardAgent = true;
             addKeysToAgent = "yes";
+            compression = true;
+            hashKnownHosts = true;
+            extraConfig = ''
+              Host github.com
+                HostName github.com
+                User git
+                IdentityFile ~/.ssh/id_ed25519
+
+              Host github-work
+                HostName github.com
+                User git
+                IdentityFile ~/.ssh/github_work
+            '';
           };
           kitty = {
             enable = true;
