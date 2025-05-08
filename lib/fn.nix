33302fa5fb76b07de76529b96f9bf13c1f8e36ed
god464 <god464@users.noreply.github.com>
2025-05-08 10:04:54 +0800

    fix: fix

--- lib/fn.nix
+++ lib/fn.nix
@@ -1,17 +1,9 @@
 { lib, ... }:
-rec {
-
-  # importModule' =
-  #   path:
-  #   builtins.readDir path
-  #   |> lib.attrNames
-  #   |> lib.subtractLists [ "default.nix" ]
-  #   |> map (n: path + "/${n}");
-  getFileNames = { path, exclude }: lib.subtractLists exclude (lib.attrNames (builtins.readDir path));
+{
   importModule' =
     path:
-    map (n: path + "/${n}") (getFileNames {
-      inherit path;
-      exclude = [ "default.nix" ];
-    });
+    builtins.readDir path
+    |> lib.attrNames
+    |> lib.subtractLists [ "default.nix" ]
+    |> map (n: path + "/${n}");
 }
