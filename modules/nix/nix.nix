aa6ec972a2465dc2bc2bb40eb1e672ca290818bd
god464 <god464@users.noreply.github.com>
2025-05-23 18:02:09 +0800

    chore: use cernet

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -37,7 +37,7 @@ in
         substituters = mkAfter (
           cfg.cache
           ++ [
-            "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
+            "https://mirrors.cernet.edu.cn/nix-channels/store"
             "https://nix-community.cachix.org"
             "https://numtide.cachix.org"
             "https://cache.garnix.io"
