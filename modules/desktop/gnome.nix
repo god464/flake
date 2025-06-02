c5a50f67eab0192f77a6f0968828a5fa9a94ad77
god464 <god464@users.noreply.github.com>
2025-06-02 13:13:43 +0800

    feat: feat

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -15,6 +15,34 @@ in
       displayManager.gdm.enable = true;
       desktopManager.gnome.enable = true;
     };
+    programs.dconf = {
+      enable = true;
+      profiles.user.databases = [
+        {
+          lockAll = true;
+          settings = {
+            "org/gnome/desktop/interface" = {
+              clock-show-seconds = true;
+              clock-show-weekday = true;
+            };
+            "org/gnome/mutter".experimental-features = [ "scale-monitor-framebuffer" ];
+            "org/gnome/desktop/datetime".automatic-timezone = true;
+            "org/gnome/shell".favorite-apps = [
+              "firefox.desktop"
+              "thunderbird.desktop"
+              "org.gnome.Nautilus.desktop"
+            ];
+            "org/gnome/desktop/peripherals/touchpad".two-finger-scrolling-enabled = true;
+            "org/gnome/desktop/screen-time-limits".daily-limit-enabled = true;
+            "org/gnome/desktop/break-reminders/movement" = {
+              duration-seconds = 300;
+              interval-seconds = 1800;
+              play-sound = true;
+            };
+          };
+        }
+      ];
+    };
     environment = {
       gnome.excludePackages = with pkgs; [
         gnome-weather
@@ -31,7 +59,10 @@ in
       ];
       systemPackages =
         with pkgs;
-        [ gnome-tweaks ]
+        [
+          gnome-tweaks
+          wl-clipboard
+        ]
         ++ (with pkgs.gnomeExtensions; [
           paperwm
           blur-my-shell
