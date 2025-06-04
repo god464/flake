11813fa5af720543aeb4cb668e2c2188be41c98b
god464 <god464@users.noreply.github.com>
2025-06-04 12:26:18 +0800

    refactor: refactor

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -15,70 +15,62 @@ in
     services = {
       displayManager.gdm.enable = true;
       desktopManager.gnome.enable = true;
+      gnome = {
+        evolution-data-server.enable = true;
+        gnome-keyring.enable = true;
+        sushi.enable = true;
+      };
     };
-    programs.dconf = {
-      enable = true;
-      profiles.user.databases = [
-        {
-          lockAll = true;
-          settings = {
-            "org/gnome/shell" = {
-              disable-user-extensions = false;
-              enabled-extensions = [ "paperwm@paperwm.github.com" ];
+    programs = {
+      evolution.enable = true;
+      dconf = {
+        enable = true;
+        profiles.user.databases = [
+          {
+            lockAll = true;
+            settings = {
+              "org/gnome/desktop/interface" = {
+                clock-show-seconds = true;
+                clock-show-weekday = true;
+              };
+              "org/gnome/mutter".experimental-features = [ "scale-monitor-framebuffer" ];
+              "org/gnome/desktop/datetime".automatic-timezone = true;
+              "org/gnome/shell".favorite-apps = [
+                "firefox.desktop"
+                "thunderbird.desktop"
+                "org.gnome.Nautilus.desktop"
+              ];
+              "org/gnome/desktop/peripherals/touchpad".two-finger-scrolling-enabled = true;
+              "org/gnome/desktop/screen-time-limits".daily-limit-enabled = true;
+              "org/gnome/desktop/break-reminders/movement" = {
+                duration-seconds = mkUint32 300;
+                interval-seconds = mkUint32 1800;
+                play-sound = true;
+              };
             };
-            "org/gnome/desktop/interface" = {
-              clock-show-seconds = true;
-              clock-show-weekday = true;
-            };
-            "org/gnome/mutter".experimental-features = [ "scale-monitor-framebuffer" ];
-            "org/gnome/desktop/datetime".automatic-timezone = true;
-            "org/gnome/shell".favorite-apps = [
-              "firefox.desktop"
-              "thunderbird.desktop"
-              "org.gnome.Nautilus.desktop"
-            ];
-            "org/gnome/desktop/peripherals/touchpad".two-finger-scrolling-enabled = true;
-            "org/gnome/desktop/screen-time-limits".daily-limit-enabled = true;
-            "org/gnome/desktop/break-reminders/movement" = {
-              duration-seconds = mkUint32 300;
-              interval-seconds = mkUint32 1800;
-              play-sound = true;
-            };
-            "org/gnome/settings-daemon/plugins/media-keys" = {
-              screensaver = [ "<Super>Escape" ];
-            };
-            "org/gnome/shell/extensions/paperwm/keybindings" = {
-              switch-down = [ "<Super>j" ];
-              switch-left = [ "<Super>h" ];
-              switch-right = [ "<Super>l" ];
-              switch-up = [ "<Super>k" ];
-              toggle-scratch-window = [ "<Super>m" ];
-            };
-          };
-        }
-      ];
-    };
-    environment = {
-      gnome.excludePackages = with pkgs; [
-        gnome-weather
-        gnome-maps
-        geary
-        gnome-music
-        gnome-tour
-        gnome-text-editor
-        gnome-console
-        gnome-contacts
-        simple-scan
-        snapshot
-        yelp
-      ];
-      systemPackages =
-        with pkgs;
-        [
+          }
+        ];
+      };
+      environment = {
+        gnome.excludePackages = with pkgs; [
+          gnome-weather
+          gnome-maps
+          geary
+          gnome-music
+          gnome-tour
+          gnome-text-editor
+          gnome-console
+          gnome-contacts
+          gnome-characters
+          simple-scan
+          snapshot
+          yelp
+        ];
+        systemPackages = with pkgs; [
           gnome-tweaks
           wl-clipboard
-        ]
-        ++ (with pkgs.gnomeExtensions; [ paperwm ]);
+        ];
+      };
     };
   };
 }
