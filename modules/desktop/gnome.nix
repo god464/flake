f18129136f1dc19364cc688543ffd83515b693bb
god464 <god464@users.noreply.github.com>
2025-06-04 12:32:32 +0800

    fix: fix

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -51,26 +51,26 @@ in
           }
         ];
       };
-      environment = {
-        gnome.excludePackages = with pkgs; [
-          gnome-weather
-          gnome-maps
-          geary
-          gnome-music
-          gnome-tour
-          gnome-text-editor
-          gnome-console
-          gnome-contacts
-          gnome-characters
-          simple-scan
-          snapshot
-          yelp
-        ];
-        systemPackages = with pkgs; [
-          gnome-tweaks
-          wl-clipboard
-        ];
-      };
+    };
+    environment = {
+      gnome.excludePackages = with pkgs; [
+        gnome-weather
+        gnome-maps
+        geary
+        gnome-music
+        gnome-tour
+        gnome-text-editor
+        gnome-console
+        gnome-contacts
+        gnome-characters
+        simple-scan
+        snapshot
+        yelp
+      ];
+      systemPackages = with pkgs; [
+        gnome-tweaks
+        wl-clipboard
+      ];
     };
   };
 }
