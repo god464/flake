0d6b64a8f1e028c1659f640f3abd0b5fe3844694
god464 <god464@users.noreply.github.com>
2025-06-09 11:11:10 +0800

    fix: fix

--- modules/nixos/nix/home-manager.nix
+++ modules/nixos/nix/home-manager.nix
@@ -17,6 +17,7 @@ in
       useGlobalPkgs = true;
       useUserPackages = true;
       sharedModules = [ topcfg.flake.homeModules.default ];
+      specialArgs = { inherit inputs; };
       users.cl.home.stateVersion = "25.05";
     };
   };
