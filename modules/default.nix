3446a05b2010e513cc72873020082d71247c7935
god464 <god464@users.noreply.github.com>
2025-06-09 11:01:15 +0800

    fix: fix

--- modules/default.nix
+++ modules/default.nix
@@ -1,12 +1,14 @@
 {
-  flake =
-    { lib, ... }:
-    {
-      nixosModules.default = {
+  flake = {
+    nixosModules.default =
+      { lib, ... }:
+      {
         imports = lib.importModule' ./nixos/.;
       };
-      homeModules.default = {
+    homeModules.default =
+      { lib, ... }:
+      {
         imports = lib.importModule' ./home/.;
       };
-    };
+  };
 }
