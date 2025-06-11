ce05bfd0e16de220aa180994a4e4cc972c75f5a9
god464 <god464@users.noreply.github.com>
2025-06-11 18:43:39 +0800

    fix: fix

--- modules/home/niri/stylix.nix
+++ modules/home/niri/stylix.nix
@@ -12,7 +12,7 @@ in
     stylix = {
       enable = true;
       image = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/master/wallpapers/nix-wallpaper-dracula.png";
+        url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/03c6c20be96c38827037d2238357f2c777ec4aa5/wallpapers/nix-wallpaper-dracula.png";
         hash = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
       };
     };
