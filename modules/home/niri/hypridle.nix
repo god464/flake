ee1abac72d1a803187d4c470dfc8e0073917754b
god464 <god464@users.noreply.github.com>
2025-06-12 15:54:41 +0800

    feat: feat

--- modules/home/niri/hypridle.nix
+++ modules/home/niri/hypridle.nix
@@ -1,7 +1,31 @@
-{ osConfig, lib, ... }:
+{
+  osConfig,
+  lib,
+  ...
+}:
 let
   cfg = osConfig.programs.niri;
 in
 {
-  config = lib.mkIf cfg.enable { services.hypridle.enable = true; };
+  config = lib.mkIf cfg.enable {
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          lock_cmd = "hyprlock";
+          before_sleep_cmd = "/run/current-system/systemd/bin/loginctl lock-session";
+        };
+        listener = [
+          {
+            timeout = 90;
+            on-timeout = "/run/current-system/systemd/bin/loginctl lock-session";
+          }
+          {
+            timeout = 900;
+            on-timeout = "/run/current-system/systemd/bin/systemctl suspend";
+          }
+        ];
+      };
+    };
+  };
 }
