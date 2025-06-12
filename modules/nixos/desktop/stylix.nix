b192f03ceb79d34cb8ecb849eaa665fb9dc2ac4e
god464 <god464@users.noreply.github.com>
2025-06-12 13:14:14 +0800

    fix: fix

--- modules/nixos/desktop/stylix.nix
+++ modules/nixos/desktop/stylix.nix
@@ -15,9 +15,21 @@ in
         url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/03c6c20be96c38827037d2238357f2c777ec4aa5/wallpapers/nix-wallpaper-dracula.png";
         hash = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
       };
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+      autoEnable = false;
       targets = {
-        fontconfig.enable = false;
-        font-packages.enable = false;
+        bat.enable = true;
+        chromium.enable = true;
+        fcitx5.enable = true;
+        fish.enable = true;
+        fnott.enable = true;
+        fuzzel.enable = true;
+        fzf.enable = true;
+        gnome.enable = true;
+        gtk.enable = true;
+        hyprlock.enable = true;
+        hyprpaper.enable = true;
+        starship.enable = true;
       };
     };
   };
