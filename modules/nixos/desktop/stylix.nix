dbea79fcd299ca56f5998b1e19d2cfe9ff45976e
god464 <god464@users.noreply.github.com>
2025-06-12 16:10:31 +0800

    feat: feat

--- modules/nixos/desktop/stylix.nix
+++ modules/nixos/desktop/stylix.nix
@@ -13,12 +13,14 @@ in
   config = lib.mkIf cfg.enable {
     stylix = {
       enable = true;
+      autoEnable = false;
       image = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/03c6c20be96c38827037d2238357f2c777ec4aa5/wallpapers/nix-wallpaper-dracula.png";
         hash = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
       };
       base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
-      autoEnable = false;
+      opacity.terminal = true;
+      overlays.enable = true;
       targets = {
         chromium.enable = true;
         fish.enable = true;
