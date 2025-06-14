6f8e8381d0b2efc8a70b4a9dcfa7b1f75777c408
god464 <god464@users.noreply.github.com>
2025-06-14 17:12:49 +0800

    fix: fix

--- modules/home/niri/hypridle.nix
+++ modules/home/niri/hypridle.nix
@@ -11,22 +11,20 @@ in
           lock_cmd = "pidof hyprlock || hyprlock";
           before_sleep_cmd = "/run/current-system/systemd/bin/loginctl lock-session";
         };
-        listener =
-          [
-            {
-              timeout = 120;
-              on-timeout = "/run/current-system/systemd/bin/loginctl lock-session";
-            }
-            {
-              timeout = 300;
-              on-timeout = "/run/current-system/systemd/bin/systemctl suspend";
-            }
-          ]
-          ++ map (x: [
-            {
+        listener = [
+          {
+            timeout = 120;
+            on-timeout = "/run/current-system/systemd/bin/loginctl lock-session";
+          }
+          {
+            timeout = 300;
+            on-timeout = "/run/current-system/systemd/bin/systemctl suspend";
+          }
+          (map
+            (x: {
               timeout = x;
               ontimeout = "systemd-ac-power|| systemctl hibernation";
-            }
+            })
             [
               600
               1800
@@ -34,7 +32,8 @@ in
               7200
               9000
             ]
-          ]);
+          )
+        ];
       };
     };
   };
