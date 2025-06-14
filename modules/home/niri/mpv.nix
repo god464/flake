bd063de4484e8e9ba537469db79c1cc5c4053c33
god464 <god464@users.noreply.github.com>
2025-06-14 17:10:43 +0800

    feat: feat

--- modules/home/niri/mpv.nix
+++ modules/home/niri/mpv.nix
@@ -3,7 +3,5 @@ let
   cfg = osConfig.programs.niri;
 in
 {
-  config = lib.mkIf cfg.enable {
-    programs.mpv.enable = true;
-  };
+  config = lib.mkIf cfg.enable { programs.mpv.enable = true; };
 }
--- modules/home/niri/zathura.nix
+++ modules/home/niri/zathura.nix
@@ -3,7 +3,5 @@ let
   cfg = osConfig.programs.niri;
 in
 {
-  config = lib.mkIf cfg.enable {
-    programs.zathura.enable = true;
-  };
+  config = lib.mkIf cfg.enable { programs.zathura.enable = true; };
 }
