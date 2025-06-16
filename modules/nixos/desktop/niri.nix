0849156d4c8351935a50cc16e34689145177075e
god464 <god464@users.noreply.github.com>
2025-06-16 11:35:48 +0800

    refactor(niri): use regreet

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -14,14 +14,14 @@ in
   options.desktop'.niri.enable = mkEnableOption "niri";
   config = mkIf cfg.enable {
     nixpkgs.overlays = [ inputs.niri-flake.overlays.niri ];
-    programs.niri = {
-      enable = true;
-      package = pkgs.niri-unstable;
+    programs = {
+      niri = {
+        enable = true;
+        package = pkgs.niri-unstable;
+      };
+      regreet.enable = true;
     };
-    services = {
-      displayManager.gdm.enable = true;
-      gnome.sushi.enable = true;
-    };
+    services.gnome.sushi.enable = true;
     security.pam.services.hyprlock.fprintAuth = false;
     environment.systemPackages = with pkgs; [
       wl-clipboard-rs
