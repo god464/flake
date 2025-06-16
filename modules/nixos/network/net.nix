279649617745bfec569fe22d710a558996b7807e
god464 <god464@users.noreply.github.com>
2025-06-16 19:42:42 +0800

    refactor(net): use networkd globally

--- modules/nixos/network/net.nix
+++ modules/nixos/network/net.nix
@@ -1,46 +1,32 @@
 { lib, config, ... }:
 let
-  inherit (lib)
-    mkOption
-    types
-    mkMerge
-    mkIf
-    ;
+  inherit (lib) mkOption types mkIf;
   cfg = config.network'.net;
   display = config.services.displayManager;
 in
 {
   options.network'.net.name = mkOption { type = types.str; };
   config = {
-    networking = mkMerge [
-      {
-        hostName = cfg.name;
-        nftables.enable = true;
-        firewall = {
+    networking = {
+      hostName = cfg.name;
+      nftables.enable = true;
+      firewall = {
+        enable = true;
+        checkReversePath = "strict";
+        filterForward = true;
+      };
+      useNetworkd = true;
+      wireless = mkIf display.enable {
+        enabled = true;
+        iwd = {
           enable = true;
-          checkReversePath = "strict";
-          filterForward = true;
+          settings.general.AddressRandomization = "network";
         };
-      }
-      (
-        if display.enable then
-          {
-            networkmanager = {
-              enable = true;
-              enableStrongSwan = true;
-              ethernet.macAddress = "random";
-              wifi.macAddress = "ramdom";
-            };
-          }
-        else
-          {
-            useNetworkd = true;
-          }
-      )
-    ];
-    systemd.network = mkIf config.networking.useNetworkd {
+      };
+    };
+    systemd.network = {
       enable = true;
-      wait-online.enable = lib.mkForce false;
+      wait-online.enable = false;
     };
     services.resolved = {
       enable = !display.enable;
