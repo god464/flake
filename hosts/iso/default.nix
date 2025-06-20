70e5e77327ba587604869323ccd3e25f77ce4ae3
god464 <god464@users.noreply.github.com>
2025-06-20 23:47:46 +0800

    fix(iso): fix account

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -2,16 +2,9 @@
 {
   imports = [ ./disk.nix ];
   network'.net.name = "livecd";
-  services'.gpg.enable = true;
   programs'.fish.enable = true;
   programs.git.enable = true;
-  users.users.guest = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-    ];
-    createHome = true;
+  users.users.root = {
     shell = pkgs.fish;
     initialPassword = "a";
   };
