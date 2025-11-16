48f7a2519ba2a4359dd760d510720eb2c715d59e
god464 <god464@users.noreply.github.com>
2025-11-16 10:46:41 +0800

    chore: use zgc

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -52,20 +52,15 @@
     (clion.override {
       forceWayland = true;
       vmopts = ''
-        -Xms128m
-        -Xmx1024m
-        -XX:ReservedCodeCacheSize=512m
-        -XX:+IgnoreUnrecognizedVMOptions
-        -XX:+UseG1GC
-        -XX:SoftRefLRUPolicyMSPerMB=50
-        -XX:CICompilerCount=2
+        -Xms2048m
+        -Xmx4096m
+        -XX:ReservedCodeCacheSize=1024m
+        -XX:+UseZGC
         -XX:+HeapDumpOnOutOfMemoryError
         -XX:-OmitStackTraceInFastThrow
-        -ea
-        -Dsun.io.useCanonCaches=false
+        -Dsun.java2d.renderer=sun.java2d.marlin.MarlinRenderingEngine
         -Djdk.http.auth.tunneling.disabledSchemes=""
         -Djdk.attach.allowAttachSelf=true
-        -Djdk.module.illegalAccess.silent=true
         -Dkotlinx.coroutines.debug=off
         -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
         -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
@@ -77,20 +72,15 @@
     (goland.override {
       forceWayland = true;
       vmopts = ''
-        -Xms128m
-        -Xmx1024m
-        -XX:ReservedCodeCacheSize=512m
-        -XX:+IgnoreUnrecognizedVMOptions
-        -XX:+UseG1GC
-        -XX:SoftRefLRUPolicyMSPerMB=50
-        -XX:CICompilerCount=2
+        -Xms2048m
+        -Xmx4096m
+        -XX:ReservedCodeCacheSize=1024m
+        -XX:+UseZGC
         -XX:+HeapDumpOnOutOfMemoryError
         -XX:-OmitStackTraceInFastThrow
-        -ea
-        -Dsun.io.useCanonCaches=false
+        -Dsun.java2d.renderer=sun.java2d.marlin.MarlinRenderingEngine
         -Djdk.http.auth.tunneling.disabledSchemes=""
         -Djdk.attach.allowAttachSelf=true
-        -Djdk.module.illegalAccess.silent=true
         -Dkotlinx.coroutines.debug=off
         -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
         -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
