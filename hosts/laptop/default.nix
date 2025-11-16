a9c2a54f21d67039a51ee07b4aeef3024b0df4cc
god464 <god464@users.noreply.github.com>
2025-11-16 09:35:28 +0800

    ci: use magic cache

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -49,8 +49,56 @@
   desktop'.niri.enable = true;
   facter.reportPath = ./facter.json;
   environment.systemPackages = with pkgs.jetbrains; [
-    clion
-    goland
+    (clion.override {
+      forceWayland = true;
+      vmopts = ''
+        -Xms128m
+        -Xmx1024m
+        -XX:ReservedCodeCacheSize=512m
+        -XX:+IgnoreUnrecognizedVMOptions
+        -XX:+UseG1GC
+        -XX:SoftRefLRUPolicyMSPerMB=50
+        -XX:CICompilerCount=2
+        -XX:+HeapDumpOnOutOfMemoryError
+        -XX:-OmitStackTraceInFastThrow
+        -ea
+        -Dsun.io.useCanonCaches=false
+        -Djdk.http.auth.tunneling.disabledSchemes=""
+        -Djdk.attach.allowAttachSelf=true
+        -Djdk.module.illegalAccess.silent=true
+        -Dkotlinx.coroutines.debug=off
+        -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
+        -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
+        --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
+        --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
+        -javaagent:/home/cl/persist/ja-netfilter/ja-netfilter.jar=jetbrains
+      '';
+    })
+    (goland.override {
+      forceWayland = true;
+      vmopts = ''
+        -Xms128m
+        -Xmx1024m
+        -XX:ReservedCodeCacheSize=512m
+        -XX:+IgnoreUnrecognizedVMOptions
+        -XX:+UseG1GC
+        -XX:SoftRefLRUPolicyMSPerMB=50
+        -XX:CICompilerCount=2
+        -XX:+HeapDumpOnOutOfMemoryError
+        -XX:-OmitStackTraceInFastThrow
+        -ea
+        -Dsun.io.useCanonCaches=false
+        -Djdk.http.auth.tunneling.disabledSchemes=""
+        -Djdk.attach.allowAttachSelf=true
+        -Djdk.module.illegalAccess.silent=true
+        -Dkotlinx.coroutines.debug=off
+        -XX:ErrorFile=$USER_HOME/java_error_in_idea_%p.log
+        -XX:HeapDumpPath=$USER_HOME/java_error_in_idea.hprof
+        --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
+        --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
+        -javaagent:/home/cl/persist/ja-netfilter/ja-netfilter.jar=jetbrains
+      '';
+    })
     webstorm
   ];
   services' = {
