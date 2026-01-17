3bbcc565f92630969455a4fabad5b61265736a02
god464 <god464@users.noreply.github.com>
2026-01-18 00:55:08 +0800

    feat(net): add relays

--- modules/nixos/network/net.nix
+++ modules/nixos/network/net.nix
@@ -49,10 +49,17 @@ in
         http3 = true;
         ipv6_servers = true;
         require_dnssec = true;
-        sources.public-resolvers = {
-          urls = [ "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md" ];
-          cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
-          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        sources = {
+          public-resolvers = {
+            urls = [ "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md" ];
+            cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+            minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+          };
+          relays = {
+            urls = [ "https://download.dnscrypt.info/resolvers-list/v3/relays.md" ];
+            cache_file = "/var/cache/dnscrypt-proxy/relays.md";
+            minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+          };
         };
         server_names = [
           "cloudflare"
