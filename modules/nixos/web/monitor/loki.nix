b8661523885252ce119659334516e0e93510354a
god464 <god464@users.noreply.github.com>
2026-03-17 23:31:50 +0800

    feat(loki): add more cfgs

--- modules/nixos/web/monitor/loki.nix
+++ modules/nixos/web/monitor/loki.nix
@@ -1,10 +1,11 @@
 { config, lib, ... }:
 let
-  cfg = config.web'.monitor.loki;
+  cfg = config.services'.monitor.loki;
   inherit (lib) mkEnableOption mkIf;
+  lokiDir = "/var/lib/loki";
 in
 {
-  options.web'.monitor.loki.enable = mkEnableOption "loki";
+  options.services'.monitor.loki.enable = mkEnableOption "loki";
   config = mkIf cfg.enable {
     services = {
       loki = {
@@ -17,6 +18,45 @@ in
             http_listen_port = 3200;
             log_level = "warn";
           };
+          schema_config.configs = [
+            {
+              from = "2020-01-01";
+              store = "tsdb";
+              object_store = "filesystem";
+              schema = "v13";
+              index = {
+                prefix = "index_";
+                period = "24h";
+              };
+            }
+          ];
+          ingester = {
+            lifecycler = {
+              address = "127.0.0.1";
+              ring = {
+                kvstore.store = "inmemory";
+                replication_factor = 1;
+              };
+              final_sleep = "0s";
+            };
+            chunk_idle_period = "5m";
+            chunk_retain_period = "30s";
+          };
+          storage_config = {
+            tsdb_shipper = {
+              active_index_directory = "${lokiDir}/tsdb-index";
+              cache_location = "${lokiDir}/tsdb-cache";
+              cache_ttl = "24h";
+            };
+            filesystem.directory = "${lokiDir}/chunks";
+          };
+          limits_config.retention_period = "168h";
+          compactor = {
+            working_directory = lokiDir;
+            retention_enabled = true;
+            delete_request_store = "filesystem";
+            compactor_ring.kvstore.store = "inmemory";
+          };
         };
       };
       promtail = {
@@ -27,7 +67,29 @@ in
             grpc_listen_port = 0;
             log_level = "warn";
           };
-          scrape_configs = [ ];
+          clients = [ { url = "http://127.0.0.1:3200/loki/api/v1/push"; } ];
+          scrape_configs = [
+            {
+              job_name = "journal";
+              journal = {
+                json = true;
+                max_age = "24h";
+                labels.job = "systemd-journal";
+              };
+            }
+            {
+              job_name = "syslog";
+              static_configs = [
+                {
+                  targets = [ "localhost" ];
+                  labels = {
+                    job = "varlogs";
+                    __path__ = "/var/log/*.log";
+                  };
+                }
+              ];
+            }
+          ];
         };
       };
       nginx.virtualHosts.localhost.locations."/loki/".proxyPass = "http://localhost:3200";
