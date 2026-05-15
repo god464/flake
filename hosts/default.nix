d097e23832640de24779d6ba673af6d05605e1a8
god464 <god464@users.noreply.github.com>
2026-05-15 21:33:47 +0800

    refactor: refactor

--- hosts/default.nix
+++ hosts/default.nix
@@ -7,35 +7,38 @@
   ...
 }:
 {
-  flake = withSystem "x86_64-linux" (
-    { pkgs, ... }:
-    {
-      nixosConfigurations =
-        let
-          commonModules = with inputs; [
+  flake = withSystem "x86_64-linux" (_: {
+    nixosConfigurations =
+      let
+        commonModules =
+          with inputs;
+          [
             disko.nixosModules.disko
             sops-nix.nixosModules.sops
             nixos-facter-modules.nixosModules.facter
             self.nixosModules.default
+          ]
+          ++ [
+            (_: {
+              nixpkgs.pkgs = withSystem "x86_64-linux" ({ pkgs, ... }: pkgs);
+            })
           ];
-          mkConfig =
-            host:
-            inputs.nixpkgs.lib.nixosSystem {
-              inherit pkgs;
-              specialArgs = {
-                inherit inputs;
-                topcfg = config;
-              };
-              modules = commonModules ++ [ ./${host} ];
+        mkConfig =
+          host:
+          inputs.nixpkgs.lib.nixosSystem {
+            specialArgs = {
+              inherit inputs;
+              topcfg = config;
             };
-        in
-        lib.genAttrs [ "laptop" "server" "iso" ] mkConfig
-        // {
-          minimal = inputs.nixpkgs.lib.nixosSystem {
-            modules = [ ./minimal ];
-            specialArgs = { inherit inputs; };
+            modules = commonModules ++ [ ./${host} ];
           };
+      in
+      lib.genAttrs [ "laptop" "server" "iso" ] mkConfig
+      // {
+        minimal = inputs.nixpkgs.lib.nixosSystem {
+          modules = [ ./minimal ];
+          specialArgs = { inherit inputs; };
         };
-    }
-  );
+      };
+  });
 }
