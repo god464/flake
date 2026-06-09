38c070a8bbbcd6e564cfebda7d5b85ed940f03dc
god464 <god464@users.noreply.github.com>
2026-06-09 20:01:43 +0800

    fix: rename

--- modules/home/niri/noctalia.nix
+++ modules/home/niri/noctalia.nix
@@ -8,28 +8,8 @@ let
   cfg = osConfig.programs.niri;
 in
 {
-  programs.noctalia-shell = lib.mkIf cfg.enable {
+  programs.noctalia = lib.mkIf cfg.enable {
     enable = true;
     package = inputs.noctalia.packages."x86_64-linux".default;
-    plugins = {
-      sources = [
-        {
-          enabled = true;
-          name = "Official Noctalia Plugins";
-          url = "https://github.com/noctalia-dev/noctalia-plugins";
-        }
-      ];
-      states = {
-        polkit-agent = {
-          enabled = true;
-          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
-        };
-        privacy-indicator = {
-          enabled = true;
-          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
-        };
-      };
-      version = 2;
-    };
   };
 }
