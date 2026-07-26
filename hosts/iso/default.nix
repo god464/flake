079430b897ba3f76ae7ada1b6c1f2ab1db3fe390
god464 <god464@users.noreply.github.com>
2026-07-26 16:51:10 +0800

    feat: feat

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -1,35 +1,66 @@
 { pkgs, ... }:
 {
-  nixpkgs.hostPlatform = "x86_64-linux";
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    hostPlatform = "x86_64-linux";
+    config.allowUnfree = true;
+  };
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   fileSystems."/" = {
     device = "/dev/root";
     fsType = "tmpfs";
   };
-  boot.loader.limine = {
-    enable = true;
-    efiSupport = true;
-    biosSupport = true;
+  boot = {
+    loader.limine = {
+      enable = true;
+      efiSupport = true;
+      biosSupport = true;
+    };
+    initrd.allowMissingModules = true;
+    supportedFilesystems = [
+      "btrfs"
+      "reiserfs"
+      "vfat"
+      "f2fs"
+      "xfs"
+      "ntfs"
+      "cifs"
+    ];
+    kernelPackages = pkgs.linuxPackages_latest;
   };
-  networking.hostName = "livecd";
+  networking = {
+    hostName = "livecd";
+    wireless.iwd.enable = true;
+  };
   programs.fish.enable = true;
-  boot.initrd = {
-    systemd.enable = false;
-    allowMissingModules = true;
-  };
   programs.git.enable = true;
   users.users.nixos = {
     isNormalUser = true;
     initialHashedPassword = "";
     shell = pkgs.fish;
   };
-  security.polkit.enable = true;
-  security.sudo.wheelNeedsPassword = false;
+  security = {
+    polkit.enable = true;
+    sudo.wheelNeedsPassword = false;
+  };
   nix.settings.trusted-users = [ "nixos" ];
-  services.getty.autologinUser = "nixos";
   hardware = {
     enableAllFirmware = true;
     enableAllHardware = true;
   };
-  services.btrfs.autoScrub.enable = false;
+  services = {
+    btrfs.autoScrub.enable = false;
+    getty.autologinUser = "nixos";
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = true;
+        PermitRootLogin = "yes";
+      };
+    };
+  };
+  time.timeZone = "Asia/Hong_Kong";
+  i18n.defaultLocale = "en_US.UTF-8";
 }
